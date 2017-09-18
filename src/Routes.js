9ca93f8e68f097b3d8dff97a3d4680de7bcb7fec
pedrogyn <pedro.paulods@hotmail.com>
2017-09-18 02:24:00 -0300

    map add

--- src/Routes.js
+++ src/Routes.js
@@ -7,9 +7,9 @@ import InicioScreen from './screens/InicioScreen';
 export default props => (
     <Router>
         <Stack key='root'>
+            <Scene key='inicio' component={InicioScreen} title="Inicio" type='reset'/>
             <Scene key='login' component={LoginScreen} title="Login" hideNavBar/>
             <Scene key='cadastro' component={CadastroScreen} title="" navTransparent/>
-            <Scene key='inicio' component={InicioScreen} title="Inicio" type='reset'/>
         </Stack>
     </Router>
 );
